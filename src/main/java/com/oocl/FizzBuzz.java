c613515760c3fd94cd9f8ee043e0713bae5bf65a
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-08 16:08:38 +0800

    passed the test - should_return_Fizz_when_input_is_multiple_of_3

--- src/main/java/com/oocl/FizzBuzz.java
+++ src/main/java/com/oocl/FizzBuzz.java
@@ -3,6 +3,9 @@ package com.oocl;
 public class FizzBuzz {
 
     public String play(int input) {
+        if(input%3==0){
+            return "Fizz";
+        }
         return ""+input;
     }
 }
--- src/test/java/com/oocl/FizzBuzzTest.java
+++ src/test/java/com/oocl/FizzBuzzTest.java
@@ -9,7 +9,14 @@ public class FizzBuzzTest {
     @Test
     public void should_return_value_when_input_is_normal(){
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String returnValue=  fizzBuzz.play(1);
+        String returnValue = fizzBuzz.play(1);
         assertEquals("1", returnValue);
+    }
+
+    @Test
+    public void should_return_Fizz_when_input_is_multiple_of_3(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String returnValue = fizzBuzz.play(3);
+        assertEquals("Fizz", returnValue);
     }
 }
