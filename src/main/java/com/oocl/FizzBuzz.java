e6036a04a32862b3bb4e8a05ca5224a33d9f4b28
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-08 16:18:32 +0800

    formating

--- src/main/java/com/oocl/FizzBuzz.java
+++ src/main/java/com/oocl/FizzBuzz.java
@@ -4,12 +4,12 @@ public class FizzBuzz {
 
     public String play(int input) {
         String outputString = "";
-        if(input % 3 == 0){
+        if (input % 3 == 0) {
             outputString += "Fizz";
         }
-        if(input % 5 == 0){
+        if (input % 5 == 0) {
             outputString += "Buzz";
         }
-        return outputString.length()>0 ? outputString : ""+input;
+        return outputString.length() > 0 ? outputString : "" + input;
     }
 }
--- src/test/java/com/oocl/FizzBuzzTest.java
+++ src/test/java/com/oocl/FizzBuzzTest.java
@@ -7,28 +7,28 @@ import static org.junit.Assert.assertEquals;
 public class FizzBuzzTest {
 
     @Test
-    public void should_return_value_when_input_is_normal(){
+    public void should_return_value_when_input_is_normal() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String returnValue = fizzBuzz.play(1);
         assertEquals("1", returnValue);
     }
 
     @Test
-    public void should_return_Fizz_when_input_is_multiple_of_3(){
+    public void should_return_Fizz_when_input_is_multiple_of_3() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String returnValue = fizzBuzz.play(3);
         assertEquals("Fizz", returnValue);
     }
 
     @Test
-    public void should_return_Buzz_when_input_is_multiple_of_5(){
+    public void should_return_Buzz_when_input_is_multiple_of_5() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String returnValue = fizzBuzz.play(5);
         assertEquals("Buzz", returnValue);
     }
 
     @Test
-    public void should_return_FizzBuzz_when_input_is_multiple_of_3_and_5(){
+    public void should_return_FizzBuzz_when_input_is_multiple_of_3_and_5() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String returnValue = fizzBuzz.play(15);
         assertEquals("FizzBuzz", returnValue);
